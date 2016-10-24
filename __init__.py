70d5267124f30abc15468abaf943d7bcbf1a55ec
Alessio Moretti <odysseus@mbp-di-odysseus.homenet.telecomitalia.it>
2016-10-24 16:54:04 +0200

    Add __init__.py

---
+++ __init__.py
@@ -0,0 +1,20 @@
+"""
+This is the startup script for the botnet. Please note that the software is developed under 
+Python 3.5.x version of the interpreter. 
+
+Authors:    Alessio "Tyrell" Moretti & Federico "Elliot" Vagnoni
+Version:    0.1.0
+Created:    24 October 2016
+Modilfied:  24 October 2016
+"""
+
+
+if __name__ == "__main__":
+    print("-------------------------------------------------------------")
+    print("            ALLSAFE BOTNET - Academic purpose only           ")
+    print("                          v. 0.1.0                           ")
+    print("\nDeveloped with no harmful intentions and love by:          ")
+    print("  - Alessio 'Tyrell' Moretti                                 ")
+    print("  - Federico 'Elliot' Vagnoni                                ")
+    print("-------------------------------------------------------------")
+
